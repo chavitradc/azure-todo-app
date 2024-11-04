0671cbfd976cc5e35b0431dd9edbc97fa5021bfe
Sunniva02 <148916611+Sunniva02@users.noreply.github.com>
2024-11-04 21:39:12 +0700

    Merge branch 'main' of https://github.com/chavitradc/azure-todo-app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_DELETE_KEY: process.env.NEXT_PUBLIC_DELETE_KEY,
+    NEXT_PUBLIC_GET_KEY: process.env.NEXT_PUBLIC_GET_KEY,
+    NEXT_PUBLIC_PUT_KEY: process.env.NEXT_PUBLIC_PUT_KEY,
+    NEXT_PUBLIC_POST_KEY: process.env.NEXT_PUBLIC_POST_KEY,
+    
+  },
 };
 
 export default nextConfig;
